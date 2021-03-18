4847461a8922ee5f25fda037af1640b75ff025d9
Calandrinon <georgesut@yahoo.com>
2021-03-18 08:37:58 +0200

    Add Lab-5

--- Lab-5/README.md
+++ Lab-5/README.md
@@ -2,3 +2,4 @@
 
 Task: Write a javascript which implements a drop-down menu with 5 principal submenus and between 3 and 5 commponents for each submenu. 
 
+![Screenshot](screenshot.png)
---
+++ Lab-5/main.js
@@ -0,0 +1,25 @@
+var menu_link =  document.querySelectorAll(".menu_link");
+var submenu_a = document.querySelectorAll(".submenu_a");
+
+menu_link.forEach(function(aitem){
+
+		aitem.addEventListener("click", function(){
+			menu_link.forEach(function(aitem){
+				aitem.classList.remove("active");
+			})
+			submenu_a.forEach(function(submenu_item){
+				submenu_item.classList.remove("active");
+			})
+			aitem.classList.add("active");
+		})
+})
+
+submenu_a.forEach(function(submenu_item){
+
+		submenu_item.addEventListener("click", function(){
+			submenu_a.forEach(function(submenu_item){
+				submenu_item.classList.remove("active");
+			})
+			submenu_item.classList.add("active");
+		})
+})
