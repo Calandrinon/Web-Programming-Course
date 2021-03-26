4f670d17b644ee3ce8af06317166a24163e1d785
Calandrinon <georgesut@yahoo.com>
2021-03-26 10:10:52 +0200

    Reset container when arriving at the last slide

--- Lab-6/main.js
+++ Lab-6/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
-    $("#slider").click(function () {
-        $('#slider .slides').animate({'margin-left': '-=100vw'}, 1000);
+    var currentSlide = 1;
+    var speed = 1000;
+    var $slider = $('#slider');
+    var $slideContainer = $slider.find('.slides');
+    var $slides = $slideContainer.find('.slide');
+
+    $slider.click(function () {
+        $slideContainer.animate({'margin-left': '-=100vw'}, speed, function() {
+            currentSlide++;
+            if (currentSlide == $slides.length) {
+                currentSlide = 1;
+                $slideContainer.css('margin-left', 0);
+            }
+        });
     });
 });
