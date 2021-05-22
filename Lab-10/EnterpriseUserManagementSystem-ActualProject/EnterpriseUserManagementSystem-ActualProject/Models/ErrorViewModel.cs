baf4a8f99c70e8e29b407f1d64bf2bd84945ff58
Calandrinon <georgesut@yahoo.com>
2021-05-22 14:40:01 +0300

    Recreate the MVC in Rider properly
    I had to remove the generated authentication code.

---
+++ Lab-10/EnterpriseUserManagementSystem-ActualProject/EnterpriseUserManagementSystem-ActualProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EnterpriseUserManagementSystem_ActualProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
