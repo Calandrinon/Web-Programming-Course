e53aad285d484800f02b57c93af6161bc79d1578
Calandrinon <georgesut@yahoo.com>
2021-05-24 11:19:54 +0300

    Add the remove operation

--- Lab-10/EnterpriseUserManagementSystem-ActualProject/EnterpriseUserManagementSystem-ActualProject/Controllers/RemoveController.cs
+++ Lab-10/EnterpriseUserManagementSystem-ActualProject/EnterpriseUserManagementSystem-ActualProject/Controllers/RemoveController.cs
@@ -1,13 +1,36 @@
+using System;
+using System.Linq;
+using EnterpriseUserManagementSystem_ActualProject.Data;
+using EnterpriseUserManagementSystem_ActualProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EnterpriseUserManagementSystem_ActualProject.Controllers
 {
     public class RemoveController : Controller
     {
+        private readonly UsersContext _usersContext;
+        
+        public RemoveController(UsersContext usersContext)
+        {
+            _usersContext = usersContext;
+        } 
+        
+        
         // GET
         public IActionResult Index()
         {
             return View();
+        }
+        
+        
+        [HttpPost]
+        public IActionResult RemoveUser(string username)
+        {
+            User user = _usersContext.Users.Single(someUser => someUser.Username.Equals(username));
+
+            _usersContext.Users.Remove(user);
+            _usersContext.SaveChanges();
+            return RedirectToAction("GetUsers", "User");
         }
     }
 }
